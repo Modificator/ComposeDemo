42a497f8e553ea2917762deebbde72ce2acf0b5e
Modificator <yunshangcn@gmail.com>
2020-07-24 23:18:48 +0800

    IconToggleButton, state value

--- demo/src/main/java/cn/modificator/demo/ui/ButtonShowController.kt
+++ demo/src/main/java/cn/modificator/demo/ui/ButtonShowController.kt
@@ -1,10 +1,15 @@
 package cn.modificator.demo.ui
 
-import androidx.compose.Composable
+import androidx.compose.*
 import androidx.core.view.ViewCompat
+import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
+import androidx.ui.material.IconToggleButton
 import androidx.ui.material.Scaffold
 import androidx.ui.material.TopAppBar
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.Close
+import androidx.ui.material.icons.filled.Star
 import cn.modificator.demo.PageController
 
 class ButtonShowController: PageController() {
@@ -17,6 +22,10 @@ class ButtonShowController: PageController() {
         Scaffold(topBar = {
             TopAppBar(title = { Text(text = "Button") })
         }) {
+            var toggleState by state{false}
+            IconToggleButton(checked = toggleState, onCheckedChange = { toggleState = it } ) {
+                Icon(asset = if (toggleState) Icons.Filled.Star else Icons.Filled.Close)
+            }
 
         }
     }
